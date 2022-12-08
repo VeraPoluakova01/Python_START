4c0778ee147b9b296af2c08c08e0b3730c4817f3
Vera <tarasevich28_88@mail.ru>
2022-12-08 16:33:25 +0300

    home work 4

--- HW_4/task_1.py
+++ HW_4/task_1.py
@@ -11,3 +11,22 @@
 38
 [2, 19]
 """
+
+n = int(input('Введите N= '))
+
+# my_list = [i for i in range(1, n + 1) if n %i == 0]
+# print(my_list)
+
+def primfacs(n):
+   i = 2
+   primfac = []
+   while i * i <= n:
+       while n % i == 0:
+           primfac.append(i)
+           n = n / i
+       i = i + 1
+   if n > 1:
+       primfac.append(n)
+   return primfac
+
+print(primfacs(n))
--- HW_4/task_2.py
+++ HW_4/task_2.py
@@ -8,3 +8,17 @@
 [1, 1, 2, 3, 3, 4, 5, 5, 6, 7, 7, 8, 9, 9]
 [2, 4, 6, 8]
 """
+from random import randint
+
+number = int(input('Введите натуральное число N = '))
+my_list = [randint(0, 30) for _ in range(number)]
+print(my_list)
+my_list_sort = []
+# i = 0
+# for i in my_list:
+#     k = (my_list.count(i))
+#     if k == 1:
+#         my_list_sort.append(i)
+
+my_list_sort = [i for i in my_list if (my_list.count(i)) == 1]
+print(my_list_sort)
